505e3b424d30a71867193879c20893bbb5d752e9
lisz1012 <lisz1012@163.com>
2019-06-15 21:42:09 -0700

    Comment on cor

--- src/com/lisz/chainofresponsibility/servlet/FilterChain.java
+++ src/com/lisz/chainofresponsibility/servlet/FilterChain.java
@@ -19,6 +19,7 @@ public class FilterChain extends AbstractFilter implements Iterator<Filter> {
 	
 	@Override
 	public boolean doFilter(Request request, Response response, FilterChain fc) {
+		//  不是for或者while循环调用，而是类似于递归的挨个调用，这样能保证request 123，response 321 的顺序处理
 		if (hasNext()) {
 			next().doFilter(request, response, this);
 		} else {
