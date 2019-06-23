4b275095f44ec12fd281fe2eba6f6729c56ecdb6
lisz1012 <lisz1012@163.com>
2019-06-22 21:23:43 -0700

    Prototype deep clone, add notes

--- src/com/lisz/bridge/prototype/Test.java
+++ src/com/lisz/bridge/prototype/Test.java
@@ -10,6 +10,11 @@ public class Test {
 		System.out.println(p1.getLoc() == p2.getLoc());
 		p1.getLoc().setStreet("sh");
 		System.out.println(p2.getLoc());
+		
+		p2.setLoc((Location)p1.getLoc().clone());
+		System.out.println(p1.getLoc() == p2.getLoc());
+		p1.getLoc().setStreet("bj");
+		System.out.println(p2.getLoc());
 	}
 }
 
@@ -26,6 +31,11 @@ class Person implements Cloneable {
 	// 重写clone方法，改protected为public
 	// Object的clone方法做的就是把beiclone对象的内存内容copy并dump到另一个位置
 	// Location 引用地址被copy, 浅克隆
+	// String类型的虽然是引用类型，但因为它是在常量池中，所以不可变，改String其实只是拨动指针，指向不同的常量
+	// new String("abc")这样创造出来的String还是从堆里指向常量池中的那个String “abc”，除非能改变常量池中的
+	// “abc", 堆里的那个对象只是个中转, String 无法克隆，没实现Cloneable接口。clone了解就好.
+	// 用于属性很多，指定起来麻烦的情形。实际中用到的极少
+	// 注意⚠️如果只是重写Object 的 clone方法而没有implement Cloneable接口的话会报异常
 	@Override
 	public Object clone() throws CloneNotSupportedException {
 		return super.clone();
@@ -42,11 +52,23 @@ class Person implements Cloneable {
 	public Location getLoc() {
 		return loc;
 	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	public void setLoc(Location loc) {
+		this.loc = loc;
+	}
 	
 	
 }
 
-class Location {
+class Location implements Cloneable {
 	private String street;
 	private int roomNo;
 	public Location(String street, int roomNo) {
@@ -69,5 +91,10 @@ class Location {
 	@Override
 	public String toString() {
 		return street + " " + roomNo;
+	}
+	
+	@Override
+	public Object clone() throws CloneNotSupportedException {
+		return super.clone();
 	}
 }
